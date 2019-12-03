32bd9389e18bfc527f8acda294c4c9bd60ce077b
chryl <cyl5yy412@gmail.com>
2019-12-03 16:14:56 +0800

    init

---
+++ src/com/chryl/base/OnlyMain.java
@@ -0,0 +1,26 @@
+package com.chryl.base;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+/**
+ * Created by Chryl on 2019/12/3.
+ */
+public class OnlyMain {
+    public static void main(String[] args) {
+        //cpu核心数:逻辑,获得cpu逻辑核心数,并不是物理核心数
+        int i = Runtime.getRuntime().availableProcessors();
+        System.out.println(i);
+
+        //java虚拟机线程管理
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        //DemoTest类下的所有线程,不需要取monitor和synchronize信息,仅仅取线程和线程堆栈信息
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "]" + ":" + threadInfo.getThreadName());
+
+        }
+
+    }
+}
---
+++ src/com/chryl/base/xly/threadBase/NewThreadDemo.java
@@ -0,0 +1,41 @@
+package com.chryl.base.xly.threadBase;
+
+
+/**
+ * 线程运行本质只有两个方法,看Thread源码注解,官方只承认两种,thread 和runnable
+ * <p>
+ * Created by Chryl on 2019/8/22.
+ */
+public class NewThreadDemo {
+
+    //1
+    private static class UserThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("thread");
+        }
+    }
+
+    //2
+    private static class UserRunnable implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("runnable");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        UserThread userThread = new UserThread();
+        userThread.start();
+        //第二次start会抛异常,会判断线程的执行状态 ,start0()
+        userThread.start();
+
+        //
+        UserRunnable userRunnable = new UserRunnable();
+        Thread thread = new Thread(userRunnable);
+        thread.start();
+
+
+    }
+}
